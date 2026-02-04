f28941fafc7139205ca8bd0a8216aaf48ee6900a
harinsuvi <harddy40@gmail.com>
2026-02-04 20:03:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('loveButton');
+
+button.addEventListener('mouseover', () => {
+    const x = Math.random() * (window.innerWidth - button.offsetWidth);
+    const y = Math.random() * (window.innerHeight - button.offsetHeight);
+    button.style.position = 'absolute';
+    button.style.left = x + 'px';
+    button.style.top = y + 'px';
+});
